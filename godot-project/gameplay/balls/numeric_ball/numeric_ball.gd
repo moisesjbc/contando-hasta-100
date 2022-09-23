88b448658f8c637bc58129a10a967076780a36cc
Moisés J <moisesjbc@gmail.com>
2022-09-23 19:15:25 +0100

    Remove expected value logic from numeric balls

--- godot-project/gameplay/balls/numeric_ball/numeric_ball.gd
+++ godot-project/gameplay/balls/numeric_ball/numeric_ball.gd
@@ -4,38 +4,13 @@ extends Area2D
 signal numeric_ball_selected
 
 var value = null
-var expected_value = null
-
-
-func _update_color():
-	if value == expected_value:
-		modulate = Color.greenyellow
-	else:
-		modulate = Color.indianred
 
 
 func set_value(new_value: int):
 	value = new_value
 	$label.bbcode_text = "[center]" + str(value) + "[/center]"
-	_update_color()
-
-
-func set_expected_value(new_expected_value: int):
-	expected_value = new_expected_value
-	_update_color()
 
 
 func _on_numeric_ball_input_event(_viewport, event, _shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
 		emit_signal("numeric_ball_selected", self)
-
-
-func evaluate(expression_str: String):
-	var expression = Expression.new()
-	expression.parse(expression_str, ["x"])
-	var result = expression.execute([value])
-	set_value(result)
-
-
-func has_expected_value():
-	return value == expected_value
