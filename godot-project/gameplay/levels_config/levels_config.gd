d9913a8799bdc7239cb5eb39084f58a5b179f8ef
Moisés J <moisesjbc@gmail.com>
2022-09-24 10:25:22 +0100

    Refactor levels config structure

--- godot-project/gameplay/levels_config/levels_config.gd
+++ godot-project/gameplay/levels_config/levels_config.gd
@@ -3,20 +3,26 @@ extends Node
 const HINT_KEY = "hint"
 
 
-var level_hints = {
-	0: "¡Oh! ¡Hola!\n¿Podrías hacerme un favor y clicar en ese 0 de ahí abajo?",
-	1: "¡Guay, gracias!\n¿Y qué tal con el 1?"
-}
-
-
 var current_level_config = {
 	HINT_KEY: null
 }
 
 
+var _level_configs = {
+	0: {
+		HINT_KEY: "¡Oh! ¡Hola!\n¿Podrías hacerme un favor y clicar en ese 0 de ahí abajo?",
+	},
+	1: {
+		HINT_KEY: "¡Guay, gracias!\n¿Y qué tal con el 1?"
+	}
+}
+
+
 func get_level_config(current_level_index: int):
 	current_level_config[HINT_KEY] = null
-	if current_level_index in level_hints:
-		current_level_config[HINT_KEY] = level_hints[current_level_index]
+
+	if current_level_index in _level_configs:
+		if HINT_KEY in _level_configs[current_level_index]:
+			current_level_config[HINT_KEY] = _level_configs[current_level_index][HINT_KEY]
 
 	return current_level_config
