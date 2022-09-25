6a6b2460b4efdffa1a525c1f05f034134010fed1
Moisés J <moisesjbc@gmail.com>
2022-09-25 21:04:02 +0100

    Add music (placeholder)

--- godot-project/gui/options_menu/options_menu.gd
+++ godot-project/gui/options_menu/options_menu.gd
@@ -2,17 +2,31 @@ extends Control
 
 
 const SFX_VOLUME_RANGE = 20
+const MUSIC_VOLUME_RANGE = 20
 
 
 func _ready():
 	$center_container/panel/margin_container/vbox_container/sfx_volume_container/hbox_container/slider.value = (AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")) + SFX_VOLUME_RANGE) / (SFX_VOLUME_RANGE * 2) * 100
+	$center_container/panel/margin_container/vbox_container/music_volume_container/slider.value = (AudioServer.get_bus_volume_db(AudioServer.get_bus_index("music")) + MUSIC_VOLUME_RANGE) / (MUSIC_VOLUME_RANGE * 2) * 100
 
 
 func _on_sfx_slider_value_changed(value):
-	var volume = value / 100 * (SFX_VOLUME_RANGE * 2) - SFX_VOLUME_RANGE
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), volume)
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), volume < -19)
+	_on_volume_changed(value, "SFX", SFX_VOLUME_RANGE)
+
+
+func _on_music_slider_value_changed(value):
+	_on_volume_changed(value, "music", MUSIC_VOLUME_RANGE)
+
+
+func _on_volume_changed(value, bus_name, volume_range):
+	var volume = value / 100 * (volume_range * 2) - volume_range
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus_name), volume)
+	AudioServer.set_bus_mute(AudioServer.get_bus_index(bus_name), volume < -(volume_range - 1))
+	
 
 
 func _on_main_menu_button_pressed():
 	get_tree().change_scene("res://gui/main_menu/main_menu.tscn")
+
+
+
